6d6c7f115f8b9b4f52d74333ee7404537ca05210
Adoniro Salles <adoniro@i9tecnologiaseguros.com.br>
2023-06-08 19:41:57 -0300

    adicionando keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Estilo de Campos',
-  description: 'Conheça a pousada Estilo de Campos, um lugar tranquilo e aconchegante para você e toda sua família. Venha se hospedar com a gente.'
+  description: 'Conheça os chalés Estilo de Campos, um lugar tranquilo e aconchegante para você e toda sua família. Venha se hospedar com a gente.',
+  keywords: ['Campos do Jordão', 'Hospedagem', 'Conforto', 'Chalés', 'Pousadas', 'Natureza', 'Turismo'] 
 }
 
 export default function RootLayout({
