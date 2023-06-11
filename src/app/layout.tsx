cdb865d01a1d75aaca6a59e3b0de9faac5f13d6b
Adoniro Salles <adoniro@i9tecnologiaseguros.com.br>
2023-06-10 22:21:09 -0300

    cofiguration google tag verification and analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 
@@ -5,10 +7,14 @@ import { Nunito } from 'next/font/google'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Estilo de Campos',
   description: 'Conheça os chalés Estilo de Campos, um lugar tranquilo e aconchegante para você e toda sua família. Venha se hospedar com a gente.',
-  keywords: ['Campos do Jordão', 'Hospedagem', 'Conforto', 'Chalés', 'Pousadas', 'Natureza', 'Turismo'] 
+  keywords: ['Campos do Jordão', 'Hospedagem', 'Conforto', 'Chalés', 'Pousadas', 'Natureza', 'Turismo'],
+  verification: {
+    google: 'google-site-verification=MN4QpuRzgxDYUx5A0uwmwuFJfPRG_SDkAw1FXSdv7og'
+  },
+
 }
 
 export default function RootLayout({
@@ -18,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <body className={`${nunito.className} text-gray-800`} >{children}</body>
+      <body className={`${nunito.className} text-gray-800`} >
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
